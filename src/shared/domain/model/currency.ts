9baf67b4ae78605a7f43b5421c59835496e8717d
Ariana Ramirez <ariana190406@gmail.com>
2025-09-09 11:57:13 -0500

    feat(currency): add currency value object with format support.

--- src/shared/domain/model/currency.ts
+++ src/shared/domain/model/currency.ts
@@ -1,18 +1,57 @@
-type UpperCaseLetter = | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G';
+/**
+ * Module for handling currency codes and formatting amounts.
+ *
+ * @remarks
+ * This module defines a `Currency` class that encapsulates a currency code
+ * and provides a method to format amounts according to that currency.
+ * @module Currency
+ */
 
+/**
+ * Type representing a three-letter currency code.
+ * Each letter must be an uppercase English alphabet character (A-Z).
+ */
+type UpperCaseLetter = | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L'
+    | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
+
+/**
+ * Type alias for a three-letter currency code.
+ */
     export type CurrencyCode = `${UpperCaseLetter}-${UpperCaseLetter}${UpperCaseLetter}`;
 
+/**
+ * Value object representing a currency with a three-letter code.
+ * Provides a method to format amounts in that currency.
+ */
     export class Currency {
         private readonly _code: CurrencyCode;
 
+    /**
+     * Creates a new Currency instance.
+     * @param {CurrencyCode} code - A three-letter currency code (e.g., 'USD', 'EUR').
+     */
         constructor(code: CurrencyCode) {
             this._code = code;
         }
 
+    /**
+     * Gets the currency code.
+     * @returns {string} The three-letter currency code as a string.
+     */
         public get code():string {
             return this._code;
         }
 
+    /**
+     * Gets the currency code.
+     *
+     * @remarks
+     * This method uses the `toLocaleString` function to format the amount
+     * It defaults to 'en-US' locale if none is provided.
+     * @param {number} amount - The amount to format.
+     * @param {string} [locale='en-US'] - The locale to use for formatting {default is 'en-us'}.
+     * @returns {string} The formatted currency string.
+     */
         public forAmount = (amount: number, locale: string = 'en-US'): string => {
             return amount.toLocaleString(locale, {
                 style: 'currency',
